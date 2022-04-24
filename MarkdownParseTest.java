727a7639a8b2eb0267ecb14b24e480fd9c8bc1ff
mtonsing <mtonsing@ucsd.edu>
2022-04-24 15:30:55 -0700

    k

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,7 +6,9 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void getLinks() {\
+        //first entry will be what we want them to print
+        //second entry call the getLinks()
+        assertEquals(List.of("https://something.com","some-thing.html"),getLinks((test-file.md).readtostring));
     }
 }
