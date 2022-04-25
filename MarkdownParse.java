75a1de48447280b85fd10db5d74c2ca7e2f055d2
mtonsing <mtonsing@ucsd.edu>
2022-04-24 17:01:14 -0700

    describing the lines

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,20 +10,29 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
+        //start at index 0
         int currentIndex = 0;
+        //makesure index doesn't go out of bound
         while(currentIndex < markdown.length()) {
+            //get the index of first openBracket
             int openBracket = markdown.indexOf("[", currentIndex);
+            //get the index of last openbracket and start 
+            //to find another openbracket
             int closeBracket = markdown.indexOf("]", openBracket);
-            //System.out.println(openBracket);
+            //look for the index of an open parenthesis and then start
+            //looking for a closebracket
             int openParen = markdown.indexOf("(", closeBracket);
+            //look for closeparen and start looking for openParen
             int closeParen = markdown.indexOf(")", openParen);
+            //add markdown string starting from openParen to closeParen
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            //change currentIndex to start at closeParen
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
+            //System.out.println(currentIndex);
         }
-
+        //return what is inside in the parenthesis
         return toReturn;
-        //System.out.println(currentIndex);
+        
     }
 
 
