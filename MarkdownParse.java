f4802eaf86c93fcafa9f3cfc855989349a8e55bd
mtonsing <mtonsing@ucsd.edu>
2022-05-12 11:23:10 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
+    static int age;
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
